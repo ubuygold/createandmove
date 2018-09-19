ef11e43085c4b4f9df5a22a33e85d322a2b4ff6d
Xiao Qinghua <ubuygold@live.com>
2018-09-19 15:19:12 +0800

    从配置文件读取目标文件夹路径

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 #!/usr/bin/env python
 import os
 import shutil
@@ -7,7 +7,7 @@ ABSPATH = os.path.abspath('.')  # 获取当前的绝对地址
 # TARGETPATH = ['C:/Users/ubuyg/Documents/jiaobenlaren/target/target1', 'C:/Users/ubuyg/Documents/jiaobenlaren/target/target2', 'C:/Users/ubuyg/Documents/jiaobenlaren/target/target3']  # 目标地址，存放模拟器中电报文件夹的位置，以列表形式存放
 # TARGETPATH.sort()
 TARGET_NUMBER = 10  # 每个模拟器文件夹存放的电报文件夹个数
-DIRNUMBERS = 3  # 指定目标文件夹数量，通常是将打开的模拟器的数量 
+DIRNUMBERS = 3  # 指定目标文件夹数量，通常是将打开的模拟器的数量
 
 
 def getPhonelist(abspath):
@@ -34,27 +34,26 @@ def makeDir(abspath):
 
 def fileExist(path):
     '''检测目标文件夹是否为空，不为空则清空'''
-    if os.listdir(path) != []:
+    if os.path.exists(path):
         shutil.rmtree(path)
         os.mkdir(path)
-    else:
-        pass
-
-
+    else: 
+        os.makedirs(path)
+  
 def getTargetPath(abspath):
     '''从文本文件中获取目标文件夹'''
     targetDir = []
     if os.path.exists(os.path.join(abspath, 'targetPath.txt')) != True:
-        with open (os.path.join(abspath, 'targetPath.txt'), 'a') as f:
+        with open(os.path.join(abspath, 'targetPath.txt'), 'a') as f:
             f.write('')
     else:
-        with open (os.path.join(abspath, 'targetPath.txt'), 'r') as r:
+        with open(os.path.join(abspath, 'targetPath.txt'), 'r') as r:
             for line in r.readlines():
                 targetDir.append(line.strip())
     targetDir.sort()
     return targetDir
-            
-                
+
+
 def doCopyAndMove(phonelist, abspath):
     '''从电报文件夹列表中获取一个，并将该文件夹存放到临时文件夹和归档文件夹，并在临时文件夹中创建文本文件记录'''
     DIRNAME = phonelist.pop()
@@ -71,7 +70,7 @@ def doCopyAndMove(phonelist, abspath):
 def doMoveToTarDirs(targetpath, abspath):
     '''将临时文件夹中的所有文件移动到目标文件夹'''
     targetDir = targetpath.pop()
-    fileExist(targetDir)
+    print(targetDir)
     for list in os.listdir(os.path.join(abspath, 'current/')):
         absfilepath = os.path.join(abspath, 'current/', list)
         shutil.move(absfilepath, targetDir)
@@ -93,5 +92,7 @@ if __name__ == '__main__':
     makeDir(ABSPATH)
     TARGETPATH = getTargetPath(ABSPATH)
     PHONELIST = getPhonelist(ABSPATH)
+    for line in TARGETPATH:
+        fileExist(line)
     doCount(TARGET_NUMBER, PHONELIST, TARGETPATH, ABSPATH, DIRNUMBERS)
     input('Press Enter')
